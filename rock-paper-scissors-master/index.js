4b3c99a27c0e31c4dd674e4e759ea7a6ddf28494
Ejiro <107992530+EjiroOsiephri@users.noreply.github.com>
2023-02-17 20:10:42 +0100

    completed mobile design

--- rock-paper-scissors-master/index.js
+++ rock-paper-scissors-master/index.js
@@ -297,3 +297,160 @@ if (computerChoice === "rock") {
   result = "You win!";
 }
 // results
+
+// adding event listeners for paper
+
+paper.addEventListener("click", () => {
+  const computerChoiceNum = Math.floor(Math.random() * 3) + 1;
+  const img = document.createElement("img");
+
+  // adding winning or losing option
+
+  if (computerChoiceNum === 1) {
+    const drawText = document.createElement("h1");
+    const rockButton = document.createElement("button");
+    rockButton.innerText = "PLAY AGAIN";
+    rockButton.className = "rockbtn";
+
+    countValue.innerText = countValue.innerText;
+    drawText.className = "drawtext";
+
+    computerContribution.append(drawText, rockButton);
+    rules.style.marginTop = "280px";
+    drawText.textContent = arrayMethod[0];
+
+    drawText.style.marginTop = "30px";
+    drawText.style.marginLeft = "28px";
+    const rockbtn = document.querySelector(".rockbtn");
+
+    countValue.innerText = parseInt(countValue.innerText) + 1;
+    localStorage.setItem("countValue", countValue.innerText);
+
+    rockButton.style.marginLeft = "5px";
+
+    rockbtn.addEventListener("click", () => {
+      location.reload();
+    });
+  } else if (computerChoiceNum === 2) {
+    const drawText = document.createElement("h1");
+    const rockButton = document.createElement("button");
+    rockButton.innerText = "PLAY AGAIN";
+    rockButton.style.marginLeft = "-65px";
+    rockButton.className = "rockbtn";
+
+    drawText.className = "drawtext";
+    drawText.textContent = arrayMethod[2];
+
+    drawText.style.marginTop = "30px";
+    drawText.style.marginLeft = "-60px";
+
+    computerContribution.append(drawText, rockButton);
+    rules.style.marginTop = "300px";
+
+    const rockbtn = document.querySelector(".rockbtn");
+
+    rockbtn.addEventListener("click", () => {
+      location.reload();
+    });
+  } else if (computerChoiceNum === 3) {
+    const drawText = document.createElement("h1");
+
+    const rockButton = document.createElement("button");
+
+    rockButton.innerText = "PLAY AGAIN";
+
+    rockButton.className = "rockbtn";
+
+    // const countIncrement = countValue.innerText++;
+
+    rockButton.style.marginLeft = "98px";
+    // localStorage.setItem(countValue, countValue);
+
+    drawText.textContent = arrayMethod[1];
+
+    drawText.className = "drawtext";
+
+    drawText.style.marginLeft = "90px";
+    computerContribution.append(drawText, rockButton);
+
+    rules.style.marginTop = "200px";
+
+    const rockbtn = document.querySelector(".rockbtn");
+
+    rockbtn.addEventListener("click", () => {
+      location.reload();
+    });
+  }
+
+  if (computerChoiceNum === 1) {
+    computerChoice = "rock";
+  } else if (computerChoiceNum === 2) {
+    computerChoice = "paper";
+  } else {
+    computerChoice = "scissors";
+  }
+
+  scissors.style.display = "none";
+
+  rock.style.display = "none";
+
+  imagesTriangle.style.display = "none";
+
+  firstOne.textContent = "You picked";
+
+  firstOne.style.marginTop = "-400px";
+
+  firstOne.style.marginLeft = "20px";
+
+  secondOne.textContent = "The house picked";
+
+  secondOne.style.marginLeft = "150px";
+
+  secondOne.style.marginTop = "-25px";
+
+  paper.style.marginTop = "250px";
+  paper.style.marginBottom = "-25px";
+  paper.style.marginLeft = "10px";
+
+  firstOne.textContent = "You picked";
+
+  firstOne.style.marginTop = "-400px";
+
+  firstOne.style.marginLeft = "20px";
+
+  secondOne.textContent = "The house picked";
+
+  secondOne.style.marginLeft = "150px";
+
+  secondOne.style.marginTop = "-25px";
+  console.log(computerChoiceNum, computerChoice);
+
+  computerImage.classList.add(`${computerChoice}`);
+
+  computerImage.src = `${computerChoice}.svg`;
+
+  computerContribution.style.marginTop = "170px";
+
+  if (computerChoice === "rock") {
+    computerContribution.style.marginTop = "-160px";
+    computerContribution.style.marginLeft = "100px";
+    firstOne.style.marginTop = "-120px";
+    firstOne.style.marginLeft = "-85px";
+    secondOne.style.transform = "translateX(-80px)";
+    // secondOne.style.marginBottom = "-25px";
+    secondOne.style.whiteSpace = "nowrap";
+  } else if (computerChoice === "paper") {
+    computerContribution.style.marginTop = "-100px";
+    computerContribution.style.marginLeft = "160px";
+    firstOne.style.marginTop = "-190px";
+    firstOne.style.marginLeft = "-130px";
+    secondOne.style.marginLeft = "-15px";
+  } else if (computerChoice === "scissors") {
+    computerContribution.style.marginTop = "130px";
+    computerContribution.style.marginLeft = "20px";
+    firstOne.style.marginTop = "-390px";
+    firstOne.style.marginLeft = "-10px";
+    secondOne.style.marginLeft = "145px";
+  }
+});
+
